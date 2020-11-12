85ca90c9eef0279c474b18b7dfdd1497d003d412
Priddis, Micah Lee <micah.priddis@wsu.edu>
2020-11-12 09:50:29 -0800

    Added cli

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,48 @@ mod utilities;
 mod encrypt;
 mod decrypt;
 
-fn main() {
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Opt {
+
+    // Decryption flag. If left absent then encryption will be performed
+    #[structopt(short, long)]
+    decrypt: bool,
+
+    #[structopt(parse(from_os_str))]
+    input: std::path::PathBuf,
+
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    output: std::path::PathBuf,
+
+    /// The pattern to look for
+    key: String,
+}
+
+fn main() -> io::Result<()> {
+
+    let args = Opt::from_args();
+    let mut input = File::open(args.input)?;
+    let mut output = File::create(args.output)?;
+    let mut buffer:[u8;16] = [0;16];
+    let key:Vec<u8> = utilities::decode_key(&args.key);
+
+    // read up to 10 bytes
+    let mut n = input.read(&mut buffer)?;
+    while n != 0 {
+        buffer = encrypt::encrypt(buffer, &key);
+        output.write(&buffer);
+        buffer = [0;16];
+        n = input.read(&mut buffer)?;
+    }
+    
+    Ok(())
 }
 
 
--- src/utilities.rs
+++ src/utilities.rs
@@ -1,3 +1,5 @@
+use hex::decode;
+
 pub fn word_from_bytes(bytes:[u8; 4])->u32{
     let mut word:u32 = bytes[3] as u32;
     word += (bytes[2] as u32 ) << 8;
@@ -31,6 +33,12 @@ pub fn round_string(matrix:&[[u8;4];4]){
     println!();
 }
 
+pub fn decode_key(s: &str) -> Vec<u8> {
+    if s.len() != 32 {
+        panic!("Key must be a string of 32 characters");
+    }
+    decode(s).unwrap()
+}
 
 
 //Tests
@@ -53,5 +61,11 @@ mod tests {
         assert_eq!([0x85, 0x34, 0x25, 0x91], bytes_from_word( 0x85342591 ) );
         assert_eq!([0xae, 0xce, 0x32, 0x12], bytes_from_word( 0xaece3212 ) );
         assert_eq!([0xb5, 0x00, 0x04, 0x74], bytes_from_word( 0xb5000474 ) );
+    }
+
+    #[test]
+    fn test_decode_hex() {
+        let expected:Vec<u8> = vec![0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
+        assert_eq!(expected, decode_key("2b7e151628aed2a6abf7158809cf4f3c") );
     }
 }
